0b5a7e8ce896221e719432b0ef4c9d05951dccd0
lhenriquecl <lucashenrique.cl@outlook.com>
2017-06-15 00:54:27 -0300

    Funções de formato das mensagens

--- client.py
+++ client.py
@@ -3,6 +3,137 @@
 import sys
 import socket
 import select
+import struct
+
+def getCHK(pkt):
+	CHK = ""
+	i = 0
+	for chunk in pkt:
+		if i == 8 or i == 9:
+			CHK = CHK + chunk
+		i = i + 1
+
+	return CHK
+
+def getLEN(pkt):
+	LEN = ""
+	i = 0
+	for chunk in pkt:
+		if i == 10 or i == 11:
+			LEN = LEN + chunk
+		i = i + 1
+
+	return LEN
+
+def getTYP(pkt):
+	TYP = ""
+	i = 0
+	for chunk in pkt:
+		if i == 12 or i == 13:
+			TYP = TYP + chunk
+		i = i + 1
+
+	return TYP
+
+def getID_F(pkt):
+	ID_F = ""
+	i = 0
+	for chunk in pkt:
+		if i == 14 or i == 15:
+			ID_F = ID_F + chunk
+		i = i + 1
+
+	return ID_F
+
+def getID_T(pkt):
+	ID_T = ""
+	i = 0
+	for chunk in pkt:
+		if i == 16 or i == 17:
+			ID_T = ID_T + chunk
+		i = i + 1
+
+	return ID_T
+
+def getSQN(pkt):
+	SQN = ""
+	i = 0
+	for chunk in pkt:
+		if i == 18 or i == 19:
+			SQN = SQN + chunk
+		i = i + 1
+
+	return SQN
+
+def getMSG(pkt):
+	MSG = ""
+	i = 0
+	for chunk in pkt:
+		if i > 19:
+			MSG = MSG + chunk
+		i = i + 1
+
+	return MSG
+
+def carry_around_add(a, b):
+	c = a + b
+	return(c &0xffff)+(c >>16)
+
+def checksum(msg):
+	s =0
+	for i in range(0, len(msg),2):
+		w = ord(msg[i])+(ord(msg[i+1])<<8)
+		s = carry_around_add(s, w)
+	return~s &0xffff
+
+def toBytes(var):
+	toStr = format(var, '04x')
+	return struct.pack('B',int(toStr[0]+toStr[1],16)) + struct.pack('B',int(toStr[2]+toStr[3],16))
+
+'''
+Formato do quadro:
+SYNC SYNC CHK LEN TYP ID_F ID_T SQN MSG 
+4    4    2   2   2   2    2    2
+'''
+
+def make_pkt(typeMsg, idFrom, idTo, sqNumber, msg):
+	SYNC = '\xDC\xC0\x23\xC2'
+	CHK = '\x00\x00'
+	LEN = toBytes(len(msg))
+	TYP = toBytes(typeMsg)
+	ID_F = toBytes(idFrom)
+	ID_T = toBytes(idTo)
+	SQN = toBytes(sqNumber)
+	
+	msg = map(lambda x: ord(x), msg)
+	msg = struct.pack("%dB" % len(msg), *msg)
+
+	pkt = SYNC + SYNC + CHK + LEN + TYP + ID_F + ID_T + SQN + msg
+	
+	if len(msg) % 2 != 0:
+		chk = "%04x" % checksum(SYNC + SYNC + CHK + LEN + TYP + ID_F + ID_T + SQN + '\x00' + msg)
+	else:
+		chk = "%04x" % checksum(pkt)
+
+	chkSecondByte = struct.pack('B',int(chk[0]+chk[1],16))
+	chkFirstByte = struct.pack('B',int(chk[2]+chk[3],16))
+
+	toSend = ''
+	i = 0
+	for chunk in pkt:
+		if i == 8 or i == 9:
+			if i == 8:
+				toSend = toSend + chkFirstByte
+			else:
+				toSend = toSend + chkSecondByte
+		else:
+			toSend = toSend + chunk
+		i = i + 1
+
+	# print 'Data sent: ' + ' '.join('%02X' % ord(x) for x in toSend)
+	# print ' '.join('%02X' % ord(x) for x in getMSG(toSend))
+
+	return toSend
 
 
 def chat_client():
@@ -45,8 +176,7 @@ def chat_client():
                 sys.stdout.write('>> ')
                 sys.stdout.flush()
                 msg = sys.stdin.readline()
-                s.send(msg)
-
+                s.send(make_pkt(0,0,0,0,msg))
 
 if __name__ == "__main__":
     sys.exit(chat_client())
--- server.py
+++ server.py
@@ -3,12 +3,142 @@
 import sys
 import socket
 import select
+import struct
 
 HOST = ''
 SOCKET_LIST = []
 RECV_BUFFER = 4096
 PORT = 9009
 
+def getCHK(pkt):
+	CHK = ""
+	i = 0
+	for chunk in pkt:
+		if i == 8 or i == 9:
+			CHK = CHK + chunk
+		i = i + 1
+
+	return CHK
+
+def getLEN(pkt):
+	LEN = ""
+	i = 0
+	for chunk in pkt:
+		if i == 10 or i == 11:
+			LEN = LEN + chunk
+		i = i + 1
+
+	return LEN
+
+def getTYP(pkt):
+	TYP = ""
+	i = 0
+	for chunk in pkt:
+		if i == 12 or i == 13:
+			TYP = TYP + chunk
+		i = i + 1
+
+	return TYP
+
+def getID_F(pkt):
+	ID_F = ""
+	i = 0
+	for chunk in pkt:
+		if i == 14 or i == 15:
+			ID_F = ID_F + chunk
+		i = i + 1
+
+	return ID_F
+
+def getID_T(pkt):
+	ID_T = ""
+	i = 0
+	for chunk in pkt:
+		if i == 16 or i == 17:
+			ID_T = ID_T + chunk
+		i = i + 1
+
+	return ID_T
+
+def getSQN(pkt):
+	SQN = ""
+	i = 0
+	for chunk in pkt:
+		if i == 18 or i == 19:
+			SQN = SQN + chunk
+		i = i + 1
+
+	return SQN
+
+def getMSG(pkt):
+	MSG = ""
+	i = 0
+	for chunk in pkt:
+		if i > 19:
+			MSG = MSG + chunk
+		i = i + 1
+
+	return MSG
+
+def carry_around_add(a, b):
+	c = a + b
+	return(c &0xffff)+(c >>16)
+
+def checksum(msg):
+	s =0
+	for i in range(0, len(msg),2):
+		w = ord(msg[i])+(ord(msg[i+1])<<8)
+		s = carry_around_add(s, w)
+	return~s &0xffff
+
+def toBytes(var):
+	toStr = format(var, '04x')
+	return struct.pack('B',int(toStr[0]+toStr[1],16)) + struct.pack('B',int(toStr[2]+toStr[3],16))
+
+'''
+Formato do quadro:
+SYNC SYNC CHK LEN TYP ID_F ID_T SQN MSG 
+4    4    2   2   2   2    2    2
+'''
+
+def make_pkt(typeMsg, idFrom, idTo, sqNumber, msg):
+	SYNC = '\xDC\xC0\x23\xC2'
+	CHK = '\x00\x00'
+	LEN = toBytes(len(msg))
+	TYP = toBytes(typeMsg)
+	ID_F = toBytes(idFrom)
+	ID_T = toBytes(idTo)
+	SQN = toBytes(sqNumber)
+	
+	msg = map(lambda x: ord(x), msg)
+	msg = struct.pack("%dB" % len(msg), *msg)
+
+	pkt = SYNC + SYNC + CHK + LEN + TYP + ID_F + ID_T + SQN + msg
+	
+	if len(msg) % 2 != 0:
+		chk = "%04x" % checksum(SYNC + SYNC + CHK + LEN + TYP + ID_F + ID_T + SQN + '\x00' + msg)
+	else:
+		chk = "%04x" % checksum(pkt)
+
+	chkSecondByte = struct.pack('B',int(chk[0]+chk[1],16))
+	chkFirstByte = struct.pack('B',int(chk[2]+chk[3],16))
+
+	toSend = ''
+	i = 0
+	for chunk in pkt:
+		if i == 8 or i == 9:
+			if i == 8:
+				toSend = toSend + chkFirstByte
+			else:
+				toSend = toSend + chkSecondByte
+		else:
+			toSend = toSend + chunk
+		i = i + 1
+
+	# print 'Data sent: ' + ' '.join('%02X' % ord(x) for x in toSend)
+	# print ' '.join('%02X' % ord(x) for x in getMSG(toSend))
+
+	return toSend
 
 def chat_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,7 +164,7 @@ def chat_server():
                 SOCKET_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
 
-                broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+                broadcast(server_socket, sockfd, "[%s:%s]"% addr, "entered our chatting room\n")
 
             # a message from a client, not a new connection
             else:
@@ -44,30 +174,30 @@ def chat_server():
                     data = sock.recv(RECV_BUFFER)
                     if data:
                         # there is something in the socket
-                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)
+                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ', getMSG(data))
                     else:
                         # remove the socket that's broken
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
 
                         # at this stage, no data means probably the connection has been broken
-                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                        broadcast(server_socket, sock, "Client (%s, %s)"% addr, "is offline\n")
 
                         # exception
                 except:
-                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                    broadcast(server_socket, sock, "Client (%s, %s)"% addr, "is offline\n")
                     continue
 
     server_socket.close()
 
 
 # broadcast chat messages to all connected clients
-def broadcast(server_socket, sock, message):
+def broadcast(server_socket, sock, ID_F, message):
     for socket in SOCKET_LIST:
         # send the message only to peer
         if socket != server_socket and socket != sock:
             try:
-                socket.send(message)
+                socket.send(make_pkt(0,0,0,0,ID_F+message))
             except:
                 # broken socket connection
                 socket.close()
